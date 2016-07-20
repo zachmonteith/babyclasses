e33a11d9de017e739a027d104ff353019e970f0d
zachmonteith <zachmonteith@gmail.com>
2016-07-19 18:05:52 -0700

    added readme, fixed bug in courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :require_admin!, except: (:index, :show)
+  before_action :require_admin!, except: [:index, :show]
 
 
     def index
